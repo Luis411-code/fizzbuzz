3cfe18d1c29c29b3ac09a71d3a8946fce2db3ecc
Luis411-code <lg1855226@gmail.com>
2026-05-08 19:49:24 -0400

    implementation #1
    validation of number entered throught the input printed in the console

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Conexion en Perfecto estado")
+function generar{
+    let valor = document.getElementById('name');
+    let numero = parseInt(valor);
+    console.log(numero)
+
+    if (isNaN (numero) || numero < 1) {
+        console.error("Error");
+        return;
+    }
+    console.log("Número Válido", numero);
+
+    for (let i = 0; i <= numero; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("Fizzbuzz");
+        }if (i % 3 === 0) {
+            console.log("Fizz");
+        } if (i % 5 === 0) {
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        } 
+        
+    }
+}
