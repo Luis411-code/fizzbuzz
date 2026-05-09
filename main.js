580449979f6b01eb45671830196b8e74a2ea9af7
Luis411-code <lg1855226@gmail.com>
2026-05-09 06:50:46 -0400

    implementation #2
    creation of conditionals for the operation of fizzbuzz

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-function generar{
-    let valor = document.getElementById('name');
-    let numero = parseInt(valor);
-    console.log(numero)
 
-    if (isNaN (numero) || numero < 1) {
+function generar(){
+    let valor = document.getElementById('numero').value;
+    let numero = parseInt(valor);
+
+
+    if (isNaN(numero) || numero < 1){
         console.error("Error");
         return;
     }
     console.log("Número Válido", numero);
 
-    for (let i = 0; i <= numero; i++) {
+    for (let i = 1; i <= numero; i++) {
+
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("Fizzbuzz");
-        }if (i % 3 === 0) {
+        }else if (i % 3 === 0) {
             console.log("Fizz");
-        } if (i % 5 === 0) {
+        }else if (i % 5 === 0) {
             console.log("Buzz");
         }else{
             console.log(i);
