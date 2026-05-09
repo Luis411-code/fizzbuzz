07469c68d8c641933d21b32e9f06706f359b00e0
Luis411-code <lg1855226@gmail.com>
2026-05-09 07:56:44 -0400

    implementation #3
    Fizzbuzz in web interface and console + some css modifications to make it look better.

--- main.js
+++ main.js
@@ -2,25 +2,31 @@
 function generar(){
     let valor = document.getElementById('numero').value;
     let numero = parseInt(valor);
+    let html = "";
 
 
     if (isNaN(numero) || numero < 1){
-        console.error("Error");
+        console.error("Error escribe un número por favor");
         return;
     }
     console.log("Número Válido", numero);
-
+    
     for (let i = 1; i <= numero; i++) {
 
         if (i % 3 === 0 && i % 5 === 0) {
+            html += "<p>Fizzbuzz</p>";
             console.log("Fizzbuzz");
         }else if (i % 3 === 0) {
             console.log("Fizz");
+            html += "<p>Fizz</p>";
         }else if (i % 5 === 0) {
             console.log("Buzz");
+            html += "<p>Buzz</p>";
         }else{
             console.log(i);
+            html += "<p>" + i + "</p>";
         } 
         
     }
+    document.getElementById("lista").innerHTML = html;
 }
